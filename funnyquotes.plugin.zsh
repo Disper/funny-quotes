6374284fb4ab468634981d1f14986514bca30c99
Michał Drzewiecki <michal.drzewiecki@sap.com>
2015-07-23 07:32:39 +0200

    funnyqoutes is working with 2 example quotes

---
+++ funnyquotes.plugin.zsh
@@ -0,0 +1,12 @@
+PLUGIN_NAME=funnyquotes
+DATA_FILE=funnyquotes
+FORTUNES_DIR=$ZSH_CUSTOM/plugins/$PLUGIN_NAME/fortunes
+DATA_FILE_PATH=$FORTUNES_DIR/$DATA_FILE
+
+if [ ! -f $DATA_FILE_PATH.dat ]; then
+    strfile $DATA_FILE_PATH $DATA_FILE_PATH.dat
+fi
+
+alias funny_quotes="fortune -a $FORTUNES_DIR"
+alias funny_quotes_cow="funny_quotes | cowthink"
+funny_quotes_cow
